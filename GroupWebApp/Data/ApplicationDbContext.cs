20e0864f1b4252cc02a2030775e7328d2c0269c9
Deysium <Dan.Duncan123@outlook.com>
2022-04-09 16:15:06 -0400

    a

---
+++ GroupWebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using GroupWebApp.Models;
+using Microsoft.EntityFrameworkCore;
+using RunGroopWebApp.Models;
+
+namespace GroupWebApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Race> Races { get; set; }
+        public DbSet<Club> Clubs { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+    }
+}
--- GroupWebApp/Program.cs
+++ GroupWebApp/Program.cs
@@ -1,7 +1,14 @@
+using GroupWebApp.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default Constructor"));
+});
 
 var app = builder.Build();
 
